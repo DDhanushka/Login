54dbbbb3f166f735082d2df1d12c0c36de7e2ea7
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-06-23 15:44:19 +0530

    database connection success

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$hostname = "localhost";
+    $dbname = "login";
+    $username = "root";
+
+    $conn = mysqli_connect($hostname, $username, '', $dbname);
+
+    if($conn){
+    	echo "success";
+    }else{
+    	echo "fail";
+    }
+
+
+ ?>
