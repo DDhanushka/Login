17f7b9816564f944c0e42b01d7082ab4fda6948d
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-06-23 16:06:04 +0530

    pwd encryption working

--- auth.php
+++ auth.php
@@ -6,7 +6,14 @@
 		$username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
         $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
-        $sql = "INSERT INTO user_login (username, password) VALUES ('$username','$password')";
+        $options = [
+		    // 'salt' => custom_function_for_salt(), //write your own code to generate a suitable salt
+		    'cost' => 12 // the default cost is 10
+		];
+
+        $hash = password_hash($password, PASSWORD_DEFAULT, $options);
+
+        $sql = "INSERT INTO user_login (username, password) VALUES ('$username','$hash')";
 
         if (mysqli_query($conn, $sql)) {
             header("Location: ./index.php");
